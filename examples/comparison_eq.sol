25732b10e4cb44718b714f9afefdd6f0e84e5ab5
Ben Price <ben.jules.price@gmail.com>
2024-04-28 14:50:09 +0100

    feat: add more comparison examples

--- examples/comparison_eq.sol
+++ examples/comparison_eq.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-contract Comparison {
+contract ComparisonEq {
     bool private attr;
 
     function foo(uint32 x, uint32 y) public packed {
-        attr = x < y;
+        attr = x == y;
     }
 }
---
+++ examples/comparison_lt.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract ComparisonLT {
+    bool private attr;
+
+    function foo(uint32 x, uint32 y) public packed {
+        attr = x < y;
+    }
+}
